ba107258abd7c06449033b20918c52fd5898b1cf
Ryota Takada <takada.ryo@dinamica.co.jp>
2023-11-03 17:05:58 +0900

    add/authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+
 const InvoiceSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -110,8 +112,6 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Not Implemented');
-
   try {
     await sql`
         DELETE FROM invoices WHERE id = ${id}
@@ -121,5 +121,19 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to Delete Invoice.',
     };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
