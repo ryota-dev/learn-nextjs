3689346d450509cb0573f09df1830379737b86cb
Ryota Takada <59213956+ryota-dev@users.noreply.github.com>
2023-11-04 17:23:30 +0900

    add/metadata (#13)
    Co-authored-by: Ryota Takada <takada.ryo@dinamica.co.jp>

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -9,6 +9,11 @@ import {
   CardsSkeleton,
 } from '@/app/ui/skeletons';
 import Cards from '../../ui/dashboard/cards';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
